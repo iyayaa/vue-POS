ce242723c96003801d47dad238bf5ce581a8d3d6
zhnangyilin <niliyzhang@foxmail.com>
2019-02-21 16:11:06 +0800

    左侧订单操作布局

--- src/views/Pos.vue
+++ src/views/Pos.vue
@@ -2,7 +2,30 @@
   <div class="pos">
     <div>
       <el-row>
-        <el-col :span="7" id="order-list">我是订单栏</el-col>
+        <el-col :span="7" class="pos-order" id="order-list">
+          <el-tabs stretch="true">
+            <el-tab-pane label="点餐">
+              <el-table :data="tableData" border  style="width: 100%">
+                <el-table-column prop="goodsName" label="商品"></el-table-column>
+                <el-table-column prop="count" label="量" width="50"></el-table-column>
+                <el-table-column prop="price" label="金额" width="70"></el-table-column>
+                <el-table-column label="操作" width="100" fixed="right">
+                  <template scope="scope">
+                    <el-button type="text" size="small">删除</el-button>
+                    <el-button type="text" size="small">增加</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </el-tab-pane>
+            <el-tab-pane label="挂单">挂单</el-tab-pane>
+            <el-tab-pane label="外卖">外卖</el-tab-pane>
+          </el-tabs>
+          <div class="order-btn">
+            <el-button type="warning">挂单</el-button>
+            <el-button type="danger">删除</el-button>
+            <el-button type="success">结账</el-button>
+          </div>
+        </el-col>
         <!--商品展示-->
         <el-col :span="17">我是产品栏</el-col>
       </el-row>
@@ -16,12 +39,128 @@
 export default {
   name: "Pos",
   components: {},
-  mounted: function() {
+  data() {
+    return {
+      tableData: [
+        {
+          goodsName: "可口可乐",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "香辣鸡腿堡",
+          price: 15,
+          count: 1
+        },
+        {
+          goodsName: "爱心薯条",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "甜筒",
+          price: 8,
+          count: 1
+        }
+      ]
+    };
+  },
+  mounted() {
     var orderHeight = document.body.clientHeight;
     document.getElementById("order-list").style.height = orderHeight + "px";
   }
 };
 </script>
 <style scoped>
+.pos {
+  font-size: 12px;
+}
+
+.pos-order {
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+}
+
+.order-btn {
+  margin-top: 10px;
+  text-align: center;
+}
+
+.title {
+  height: 20px;
+  border-bottom: 1px solid #d3dce6;
+  background-color: #f9fafc;
+  padding: 10px;
+}
+
+.often-goods-list ul li {
+  list-style: none;
+  float: left;
+  border: 1px solid #e5e9f2;
+  padding: 10px;
+  margin: 5px;
+  background-color: #fff;
+  cursor: pointer;
+}
+
+.goods-type {
+  clear: both;
+}
+
+.o-price {
+  color: #58b7ff;
+}
+
+.often-goods-list {
+  border-bottom: 1px solid #c0ccda;
+  height: auto;
+  overflow: hidden;
+  padding-bottom: 10px;
+  background-color: #f9fafc;
+}
+
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #e5e9f2;
+  height: auot;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+  cursor: pointer;
+}
+
+.cookList li span {
+  display: block;
+  float: left;
+}
+
+.foodImg {
+  width: 40%;
+}
+
+.foodName {
+  font-size: 18px;
+  padding-left: 10px;
+  color: brown;
+}
+
+.foodPrice {
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top: 10px;
+}
+
+.totalDiv {
+  height: auot;
+  overflow: hidden;
+  text-align: right;
+  font-size: 16px;
+  background-color: #fff;
+  border-bottom: 1px solid #e5e9f2;
+  padding: 10px;
+}
 </style>
 
