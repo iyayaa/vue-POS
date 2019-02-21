d32c5fa4948edd384b5965f8dabe3c7d8a5e5c99
zhnangyilin <niliyzhang@foxmail.com>
2019-02-21 15:16:27 +0800

    引入eleUI

--- src/views/Pos.vue
+++ src/views/Pos.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="home">
-    hello pos~~~~~
+  <div class="pos">
+    <div>
+      <el-row>
+        <el-col :span="7" id="order-list">我是订单栏</el-col>
+        <!--商品展示-->
+        <el-col :span="17">我是产品栏</el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Pos',
-  components: {
+  name: "Pos",
+  components: {},
+  mounted: function() {
+    var orderHeight = document.body.clientHeight;
+    document.getElementById("order-list").style.height = orderHeight + "px";
   }
-}
+};
 </script>
 <style scoped>
-
 </style>
 
